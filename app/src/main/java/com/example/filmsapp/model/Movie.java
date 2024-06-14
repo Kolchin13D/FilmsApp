2377cdeef0b7298e3adf0d7382577aa76da91df9
Kolchin13D <kolchin13d@gmail.com>
2024-06-14 15:44:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/filmsapp/model/Movie.java
@@ -0,0 +1,11 @@
+package com.example.filmsapp.model;
+
+import android.os.Parcelable;
+
+import androidx.databinding.BaseObservable;
+
+public class Movie extends BaseObservable implements Parcelable {
+
+
+
+}
